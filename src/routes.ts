99d9c13d48bfcce4d2be8c341a9a3755d2c612b0
Gustavo Henrique <ghenriquer10@gmail.com>
2022-07-16 15:28:59 -0300

    Criando logica para remover pedidos salvos no banco

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,7 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 import { CreateOrderController } from './controllers/order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 
 
 const router = Router();
@@ -45,6 +46,10 @@ router.post('/product', isAuthenticated, upload.single('file'), new CreateProduc
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 // --------------------------------------------------- Rotas ORDER -----------------------------------------------//
+// Criando novo pedido
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
+
+// Excluindo pedido
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router };
