f7dd2623aca4ccf1debee27df6d13ad2ac656c5e
Gustavo Henrique <ghenriquer10@gmail.com>
2022-08-15 15:42:53 -0300

    Testando servidor

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import { router } from './routes'
 
 const app = express();
 
+const port = process.env.PORT || 3333
+
 app.use(express.json())
 
 app.use(cors())
@@ -33,4 +35,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     })
 })
 
-app.listen(3333, () => console.log("Servivor online!!!"))
+app.listen(port, () => console.log("Servivor online!!!"))
